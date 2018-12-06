df01dd9f07ec665fbf86ef9fc11ed018114d7240
wxl <284743503@qq.com>
2018-12-06 23:44:04 +0800

    更新

---
+++ js/site.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+
+    $(".i_bock3_item").mouseenter(function () {
+        $(this).find(".i_bock3_item_float").css("display", "block");
+    })
+    $(".i_bock3_item").mouseleave(function () {
+        $(this).find(".i_bock3_item_float").css("display", "none");
+    })
+
+})
