8d5190ea954d893b18f2e2a8e59bf08bcebda38a
wxl <284743503@qq.com>
2018-12-09 22:41:13 +0800

    人才招聘

--- js/site.js
+++ js/site.js
@@ -7,6 +7,20 @@
     $(".i_bock3_item").mouseleave(function () {
         $(this).find(".i_bock3_item_float").css("display", "none");
     })
+    $("body").on('click', '.scroll_top', function () {
+        $('html,body').animate({ scrollTop: '0px' }, 800);
+    })
+    /*人才招聘*/
+    $(".r_bock1_btn").click(function () {
+        $(".r_bock1_btn").removeClass('r_btn_active');
+        $(this).addClass('r_btn_active');
+        //当前索引
+        var index = $(this).parent().index();
+        $(".r_job_info").css('display', 'none');
+
+        $(".r_job_info").eq(index).css('display', 'block');
+    })
+    /*人才招聘*/
 
 
 })
