faa0c6fa91212405df3f650597c368e0fc832baf
wxl <284743503@qq.com>
2018-12-25 00:02:18 +0800

    更新首页合作商，全部文案

--- js/site.js
+++ js/site.js
@@ -4,7 +4,7 @@
     $(".i_bock3_item").mouseenter(function () {
         $(this).find(".i_bock3_item_float").css("display", "block");
         $(this).find(".i_bock3_over").css("display", "block");
-        
+
     })
     $(".i_bock3_item").mouseleave(function () {
         $(this).find(".i_bock3_item_float").css("display", "none");
@@ -45,7 +45,133 @@
 
         $(".a_rightcontent").eq(index).css('display', 'block');
     })
-     
-
 
 })
+var webIndex = 1;
+var appIndex = 1;
+var webPageSize = 10;
+var appPageSize = 6;
+
+var pageCount = 0;
+function iNext() {
+    var imgs = $(".i_bock4imglist").find('img');
+    if (pageCount == 0) {
+        pageCount = imgs.length;
+    }
+    var appType = $(".i_bock4_web").css('display');
+    if (appType == "block") {
+        var currentCount = webIndex * webPageSize;
+        if (currentCount >= pageCount) {
+            //回到第一页
+            webIndex = 0;
+        }
+        //下一页
+        var nextCount = (webIndex + 1) * webPageSize;
+        var currentIndex = webIndex * webPageSize + 1;
+        //清掉旧的
+        $(".i_bock4_web .i_bock_list").html("");
+        for (var i = currentIndex; i <= nextCount; i++) {
+            if (i <= pageCount) {
+               //增加新的
+                var html = '<div class="col-sm-20 i_bock4_item">' +
+                    '<img src = "' + $(imgs[i - 1]).attr('src') + '" />' +
+                    '</div >';
+                $(".i_bock4_web .i_bock_list").append(html)
+            }
+            else {
+                break;
+            }
+        }
+        webIndex++;
+    }
+    else {
+        var currentCount = appIndex * appPageSize;
+        if (currentCount >= pageCount) {
+            //回到第一页
+            appIndex = 0;
+        }
+        //下一页
+        var nextCount = (appIndex + 1) * appPageSize;
+        var currentIndex = appIndex * appPageSize + 1;
+        //清掉旧的
+        $(".i_bock4_app .i_bock_list").html("");
+        for (var i = currentIndex; i <= nextCount; i++) {
+            if (i <= pageCount) {
+                //增加新的
+                var html = '<div class="col-xs-4 i_bock4_item">' +
+                    '<img src = "' + $(imgs[i - 1]).attr('src') + '" />' +
+                    '</div >';
+                $(".i_bock4_app .i_bock_list").append(html)
+            }
+            else {
+                break;
+            }
+        }
+        appIndex++;
+    }
+}
+//上一页
+function iPre() {
+    var imgs = $(".i_bock4imglist").find('img');
+    if (pageCount == 0) {
+        pageCount = imgs.length;
+    }
+
+    var appType = $(".i_bock4_web").css('display');
+    if (appType == "block") {
+        //向上取整
+        var maxPage = Math.ceil(pageCount / webPageSize);
+        if (webIndex == 1) {
+            //回到第一页
+            webIndex = maxPage;
+        }
+        else {
+            webIndex--;
+        }
+        //上一页
+        var nextCount = webIndex * webPageSize;
+        var currentIndex = (webIndex-1) * webPageSize+1;
+        //清掉旧的
+        $(".i_bock4_web .i_bock_list").html("");
+        for (var i = currentIndex; i <= nextCount; i++) {
+            if (i <= pageCount) {
+                //增加新的
+                var html = '<div class="col-sm-20 i_bock4_item">' +
+                    '<img src = "' + $(imgs[i - 1]).attr('src') + '" />' +
+                    '</div >';
+                $(".i_bock4_web .i_bock_list").append(html)
+            }
+            else {
+                break;
+            }
+        }
+    }
+    else {
+        //向上取整
+        var maxPage = Math.ceil(pageCount / appPageSize);
+        if (appIndex == 1) {
+            //回到第一页
+            appIndex = maxPage;
+        }
+        else {
+            appIndex--;
+        }
+        //上一页
+        var nextCount = appIndex * appPageSize;
+        var currentIndex = (appIndex - 1) * appPageSize + 1;
+        //清掉旧的
+        $(".i_bock4_app .i_bock_list").html("");
+        for (var i = currentIndex; i <= nextCount; i++) {
+            if (i <= pageCount) {
+                //增加新的
+                var html = '<div class="col-xs-4 i_bock4_item">' +
+                    '<img src = "' + $(imgs[i - 1]).attr('src') + '" />' +
+                    '</div >';
+                $(".i_bock4_app .i_bock_list").append(html)
+            }
+            else {
+                break;
+            }
+        }
+    }
+}
